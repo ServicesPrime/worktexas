3f3ff0b64747b2c4f97c189ead9c51b313a9016a
ServicesPrime <santiheras08@gmail.com>
2025-06-01 14:07:20 -0700

    Add applicant feature

---
+++ app/Models/Applicant.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Applicant extends Model
+{
+    use HasFactory;
+
+    protected $table = 'applicants';
+
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'correo',
+        'numero',
+        'car',
+    ];
+}
